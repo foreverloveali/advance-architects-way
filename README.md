3651ac3edbb147c7f4d702edaf9f145488223e2a
lironghao <1871446702@qq.com>
2020-06-10 22:23:48 +0800

    【advance-architects-way】【解决容器式单例的线程安全问题】【lironghao】

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # advance-architects-way
 退退的架构师进阶之路
+
+# 2020.06.10 
+com.alibaba.work.pattern.ContainerSingleton
+解决容器式单例的线程安全问题
---
+++ src/main/java/com/alibaba/work/pattern/ContainerSingleton.java
@@ -0,0 +1,67 @@
+package com.alibaba.work.pattern;
+
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @Desc 解决容器式单例的线程安全问题
+ * @Author 阿里P10大神退退
+ * @date 2020/6/9 23:02
+ */
+public class ContainerSingleton {
+
+    private static HashMap<String, Object> map = new HashMap<String, Object>();
+
+    private ContainerSingleton() {
+    }
+
+    /**
+     * 方法1
+     * @param className
+     * @return
+     */
+    public synchronized static Object getInstance1(String className) {
+        Object instance = null;
+        if (!map.containsKey(className)) {
+            try {
+                instance = Class.forName(className).newInstance();
+                map.put(className, instance);
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        } else {
+            instance = map.get(className);
+        }
+        return instance;
+    }
+
+    /**
+     * 方法2
+     * @param className
+     * @return
+     */
+    public static Object getInstance2(String className) {
+        synchronized (ContainerSingleton.class) {
+            Object instance = null;
+            if (!map.containsKey(className)) {
+                try {
+                    instance = Class.forName(className).newInstance();
+                    map.put(className, instance);
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                instance = map.get(className);
+            }
+            return instance;
+        }
+    }
+}
